686d7fbecfd03695ea6a571d805cbe2740784d27
flavupereira <flavupereira@gmail.com>
2024-06-14 20:48:37 -0300

    first commit

---
+++ src/test/java/com/flavio/apilivro/ApilivroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flavio.apilivro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApilivroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
